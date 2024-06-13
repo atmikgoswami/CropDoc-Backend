aef84cb595adfe19da2dc3298b57c45da5264f5a
Atmik Goswami <atmikcodes@gmail.com>
2024-06-14 00:16:50 +0530

    Modify app

--- app.py
+++ app.py
@@ -1,15 +1,61 @@
-from gevent import monkey
-monkey.patch_all()
-
-import torch
-import pickle
 import numpy as np
+import pickle
+import tensorflow as tf
 from PIL import Image
 from flask import Flask, request, jsonify
-import torchvision.transforms as transforms
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
+
+app = Flask(__name__)
+
+# Load TFLite model and allocate tensors
+interpreter = tf.lite.Interpreter(model_path="plant_disease_model.tflite")
+interpreter.allocate_tensors()
+
+model = pickle.load(open('modeld.pkl', 'rb'))
+
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+diseases = [
+'apple apple scab',
+'apple black rot',
+'apple cedar apple rust',
+'apple healthy',
+'blueberry healthy',
+'cherry including sour powdery mildew',
+'cherry including sour healthy',
+'corn maize cercospora leaf spot gray leaf spot',
+'corn maize common rust ',
+'corn maize northern leaf blight',
+'corn maize healthy',
+'grape black rot',
+'grape esca black measles' ,
+'grape leaf blight isariopsis leaf spot ',
+'grape healthy',
+'orange haunglongbing citrus greening ',
+'peach bacterial spot',
+'peach healthy',
+'pepper bell bacterial spot',
+'pepper bell healthy',
+'potato early blight',
+'potato late blight',
+'potato healthy',
+'raspberry healthy',
+'soybean healthy',
+'squash powdery mildew',
+'strawberry leaf scorch',
+'strawberry healthy',
+'tomato bacterial spot',
+'tomato early blight',
+'tomato late blight',
+'tomato leaf mold',
+'tomato septoria leaf spot',
+'tomato spider mites two spotted spider mite',
+'tomato target spot',
+'tomato tomato yellow leaf curl virus',
+'tomato tomato mosaic virus',
+'tomato healthy'
+]
 
 crop_mapping = {
     0: 'blackgram',
@@ -26,86 +72,6 @@ crop_mapping = {
     11: 'rice',
     12: 'watermelon',
 }
-
-diseases = ['Apple___Apple_scab',
- 'Apple___Black_rot',
- 'Apple___Cedar_apple_rust',
- 'Apple___healthy',
- 'Blueberry___healthy',
- 'Cherry_(including_sour)___Powdery_mildew',
- 'Cherry_(including_sour)___healthy',
- 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
- 'Corn_(maize)___Common_rust_',
- 'Corn_(maize)___Northern_Leaf_Blight',
- 'Corn_(maize)___healthy',
- 'Grape___Black_rot',
- 'Grape___Esca_(Black_Measles)',
- 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
- 'Grape___healthy',
- 'Orange___Haunglongbing_(Citrus_greening)',
- 'Peach___Bacterial_spot',
- 'Peach___healthy',
- 'Pepper,_bell___Bacterial_spot',
- 'Pepper,_bell___healthy',
- 'Potato___Early_blight',
- 'Potato___Late_blight',
- 'Potato___healthy',
- 'Raspberry___healthy',
- 'Soybean___healthy',
- 'Squash___Powdery_mildew',
- 'Strawberry___Leaf_scorch',
- 'Strawberry___healthy',
- 'Tomato___Bacterial_spot',
- 'Tomato___Early_blight',
- 'Tomato___Late_blight',
- 'Tomato___Leaf_Mold',
- 'Tomato___Septoria_leaf_spot',
- 'Tomato___Spider_mites Two-spotted_spider_mite',
- 'Tomato___Target_Spot',
- 'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
- 'Tomato___Tomato_mosaic_virus',
- 'Tomato___healthy']
-
-app = Flask(__name__)
-
-model = pickle.load(open('model.pkl', 'rb'))
-
-class ImageClassificationBase(nn.Module):
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)
-        loss = F.cross_entropy(out, labels)
-        return loss
-
-class Plant_Disease_Model2(ImageClassificationBase):
-    def __init__(self):
-        super().__init__()
-        self.network = models.resnet34(weights=None)
-        num_ftrs = self.network.fc.in_features
-        self.network.fc = nn.Linear(num_ftrs, 38)
-    
-    def forward(self, xb):
-        out = self.network(xb)
-        return out
-
-def get_default_device():
-    if torch.cuda.is_available():
-        return torch.device('cuda')
-    else:
-        return torch.device('cpu')
-
-def to_device(data, device):
-    if isinstance(data, (list, tuple)):
-        return [to_device(x, device) for x in data]
-    return data.to(device, non_blocking=True)
-
-device = get_default_device()
-plant_disease_model = Plant_Disease_Model2()
-plant_disease_model.load_state_dict(torch.load('plantDisease-resnet34.pth', map_location=device))
-plant_disease_model = to_device(plant_disease_model, device)
-plant_disease_model.eval()
-
-transform = transforms.Compose([transforms.Resize(size=128), transforms.ToTensor()])
 
 @app.route('/')
 def home():
@@ -144,18 +110,28 @@ def upload_file():
         # Read image from request directly
         image = Image.open(file.stream)
 
-        # Predict the disease
-        transformed_image = transform(image)
-        tensor_image = transformed_image.unsqueeze(0)
-        img = tensor_image[0]
-        predicted_disease = predict_image(img, plant_disease_model)
-        return jsonify({'prediction': predicted_disease})
+        # Prepare input image
+        input_image = image.resize((input_details[0]['shape'][1], input_details[0]['shape'][2]))
+        input_data = np.expand_dims(input_image, axis=0)
+        input_data = input_data.astype(input_details[0]['dtype'])
+        interpreter.set_tensor(input_details[0]['index'], input_data)
 
-def predict_image(img, model):
-    xb = to_device(img.unsqueeze(0), device)
-    yb = model(xb)
-    _, preds = torch.max(yb, dim=1)
-    return diseases[preds[0].item()]
+        # Run inference
+        interpreter.invoke()
+
+        # Obtain output results from the interpreter
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+
+        # Example: Assuming output is probabilities for classification
+        predicted_class_index = np.argmax(output_data)
+        confidence = output_data[0][predicted_class_index]
+
+        if confidence >= 0.4:
+            predicted_disease = diseases[predicted_class_index]
+        else:
+            predicted_disease = "Healthy Leaf"
+
+        return jsonify({'prediction': predicted_disease})
 
 if __name__ == "__main__":
     app.run()
